f84c17ab7676fdc58bd676de9cc21659a209ac22
lekaren <cpdnql@naver.com>
2019-11-12 16:48:46 +0900

    버튼, css 수정

--- router-tutorial/src/views/lateCheck.js
+++ router-tutorial/src/views/lateCheck.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../latecheck.css";
+import "../lateCheck.css";
 import "../Home.css";
 
 const lateCheck = () => {
@@ -10,6 +10,7 @@ const lateCheck = () => {
         <tr>
           <th>학번</th>
           <th>이름</th>
+          <th>정상등교</th>
           {/* 8시 50분 지각과 9시 지각을 동시에
                     체크할 수 없도록 설정해야 함 */}
           <th>8:50</th>
@@ -18,6 +19,9 @@ const lateCheck = () => {
         <tr>
           <td>21201</td>
           <td>김수아</td>
+          <td>
+            <input type="radio" name="late_01" />
+          </td>
           <td>
             <input type="radio" name="late_01" />
           </td>
@@ -34,10 +38,16 @@ const lateCheck = () => {
           <td>
             <input type="radio" name="late_02" />
           </td>
+          <td>
+            <input type="radio" name="late_02" />
+          </td>
         </tr>
         <tr>
           <td>21203</td>
           <td>김우빈</td>
+          <td>
+            <input type="radio" name="late_03" />
+          </td>
           <td>
             <input type="radio" name="late_03" />
           </td>
@@ -54,6 +64,9 @@ const lateCheck = () => {
           <td>
             <input type="radio" name="late_04" />
           </td>
+          <td>
+            <input type="radio" name="late_04" />
+          </td>
         </tr>
         <tr>
           <td>21205</td>
@@ -64,14 +77,15 @@ const lateCheck = () => {
           <td>
             <input type="radio" name="late_05" />
           </td>
+          <td>
+            <input type="radio" name="late_05" />
+          </td>
         </tr>
         {/* 확인, 취소 버튼 */}
         <tr>
-          <td colspan="2">
-            <input type="submit" value="확인" />
-          </td>
-          <td colspan="2">
-            <input type="reset" value="취소" />
+          <td colspan="5">
+            <input type="submit" class="button" value="확인" />
+            <input type="reset" class="button" value="취소" />
           </td>
         </tr>
       </table>
